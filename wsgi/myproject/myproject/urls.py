69b21910e48bd5011b6972a42c1e95cfcd713341
Javier Carrascal <javilumbrales@gmail.com>
2015-05-26 21:10:49 +0100

    Enable django rest framework

--- wsgi/myproject/myproject/urls.py
+++ wsgi/myproject/myproject/urls.py
@@ -1,10 +1,16 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
+from rest_framework import routers
+
+router = routers.DefaultRouter(trailing_slash=False)
+
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'myproject.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^api/',include(router.urls)),
+    url(r'^api-auth/',include('rest_framework.urls', namespace='rest_framework')),
 )
