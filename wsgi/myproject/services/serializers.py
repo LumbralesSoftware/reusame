62369ef78aec78207270695996c66fa1243bf76a
Javier Carrascal <javilumbrales@gmail.com>
2015-05-28 10:08:48 +0100

    Serialize Location and Category properly

--- wsgi/myproject/services/serializers.py
+++ wsgi/myproject/services/serializers.py
@@ -1,8 +1,14 @@
 from rest_framework import serializers
 from services.models import Item, Category, Location
 
-# Serializers define the API representation.
+class LocationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Location
+        fields = ('location', 'long_position', 'lat_position')
+
 class ItemSerializer(serializers.ModelSerializer):
+    location = LocationSerializer()
+    category = serializers.StringRelatedField()
     class Meta:
         model = Item
         fields = ('id', 'name', 'text', 'image', 'category', 'location', 'active')
